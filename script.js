a9bf4ebf3706dbc1f057b36a395f4bddba7ddf7d
Taynara Pecorario <taynara_rp22@hotmail.com>
2023-01-07 14:46:15 -0300

    :sparkles: send order added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let order = [];
+let textOrder;
 
 function handleSelectCard(card, type) {
   const selectedCard = document.querySelector(`.${type}.selected`);
@@ -18,14 +19,40 @@ function handleSelectCard(card, type) {
       name: card.children[1].innerText,
       value: formatedValue
     };
+  } else {
+    order.push({
+      type: type,
+      name: card.children[1].innerText,
+      value: formatedValue
+    });
   }
 
-  order.push({
-    type: type,
-    name: card.children[1].innerText,
-    value: formatedValue
-  });
+  if (order.length === 3) {
+    const button = document.querySelector('.btn');
+    button.removeAttribute('disabled');
+    button.innerText = 'Fechar pedido';
+  }
 
   card.classList.add('selected');
   card.children[4].classList.remove('hidden');
+}
+
+function handleSendOrder() {
+  const plate = order.find(item => item.type === 'plate');
+  const drink = order.find(item => item.type === 'drink');
+  const dessert = order.find(item => item.type === 'dessert');
+
+  const totalValue = order
+    .map(item => item.value)
+    .reduce((prev, curr) => Number(prev) + Number(curr), 0);
+
+  textOrder = `Olá, gostaria de fazer o pedido:
+- Prato: ${plate.name}
+- Bebida: ${drink.name}
+- Sobremesa: ${dessert.name}
+Total: R$ ${totalValue.toFixed(2)}`;
+
+  const encodeOrder = encodeURIComponent(textOrder);
+
+  window.open(`https://wa.me/5511911111111?text=${encodeOrder}`);
 }
